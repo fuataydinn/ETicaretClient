f6d4242cc66c45a2a44e2c13d6c4765a2b65adbb
fuataydinn <faydnn23@gmail.com>
2023-11-07 10:33:19 +0300

    added product_CreateService

--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
-import { Product } from 'src/app/contracts/product';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 @Component({
@@ -17,32 +16,6 @@ export class ProductsComponent  extends BaseComponent{
    }
  
    ngOnInit(): void {
- 
-     this.showSpinner(SpinnerType.BallAtom);
-     this.httpClientService.get<Product[]>({
-      controller : "products"
-     }).subscribe(data => console.log(data));
-
-     this.httpClientService.post({
-     controller : "products"
-     }, {
-      name : "Kalem2",
-      stock : 100,
-      price : 15
-     }).subscribe();
-
-    // this.httpClientService.put({
-    //   controller : "product"
-    // },{
-    //   id :"ef35134b-381a-4591-8b34-e8375eae6b53",
-    //   name :"Resim Defteri",
-    //   stock: 23,
-    //   price : 5.5
-    // }).subscribe();
-
-    // this.httpClientService.delete({
-    //   controller :"product"
-    // },"ef35134b-381a-4591-8b34-e8375eae6b53").subscribe();
 
    }
 }
